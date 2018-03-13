52866cbf2c13f45130496a06387f76b775588fcf
Ronald Daniel Ajila Calva <rajila@espol.edu.ec>
2018-03-13 20:07:44 +0100

    proyecto prueba

---
+++ pruebaMvc01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pruebaMvc01.Startup))]
+namespace pruebaMvc01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
